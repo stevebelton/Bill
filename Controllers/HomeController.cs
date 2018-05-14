030227c8943aeeddbe3200cad047049acd26fe55
Steve Belton <steve@snafoo.co.uk>
2018-05-14 15:21:20 +0100

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bill.Models;
+
+namespace Bill.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Server()
+        {
+            ViewData["Message"] = "Hey Bill, What server am I running on?";
+
+            return View();
+        }
+
+       
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Bill
+## A simple asp.net core 2.0 web application for containers
+
+
+More information for container deployment in Azure [look here](https://azure.microsoft.com/en-gb/overview/containers/).
+
+
+Have fun with Bill
